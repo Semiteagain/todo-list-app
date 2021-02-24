deba2d2e6e750808dc9d8873028170eb94ed5890
Mukaila Semiu <semiteagain@gmail.com>
2021-02-24 04:24:23 +0100

    creating todoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+
+   const [addText, setAddText] = useState('')
+   const [items, setItems] = useState([])
+
+
+   function handleChange(event) {
+     const newValue = event.target.value
+     setAddText(newValue)
+   }
+  
+   function addItem() {
+       setItems ((prevItems) =>{
+         return [
+           ...prevItems, addText
+         ]
+       });
+     setAddText('')
+   }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      Add item ... 
+      <br />
+
+      <input
+        type='text'
+        placeholder = 'Add new Item here'
+        value={addText}
+        onChange = {handleChange}
+      />
+
+      <button onClick={addItem}>Add Items</button>
+
+      <div>
+        <ul>
+              {items.map((newItem) =>{
+                return <li>{newItem}</li>
+              })}
+        </ul>
+      </div>
     </div>
+
   );
 }
 
